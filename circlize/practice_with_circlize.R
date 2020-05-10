f6650e3430711d6601a96eae91b97b5de6126b23
YinCY <yinchunyou2012@163.com>
2020-05-10 10:18:58 +0800

    circlize practice

--- circlize/practice_with_circlize.R
+++ circlize/practice_with_circlize.R
@@ -1,4 +1,7 @@
 library(circlize)
+library(tidyverse)
+
+#example 1 -------------------------------------------------------------
 n = 100
 df <- data.frame(
     fa = sample(letters[1:9], n, replace = T), 
@@ -28,9 +31,10 @@ circos.lines(x = filter(df, df$fa == "d") %>% pull(x),
              y = filter(df, df$fa == "d") %>% pull(y), 
              col = ifelse((filter(df, df$fa == "d") %>% pull(y)) > 0, "red", "green"), 
              type = "h")
+circos.clear()
 
 
-
+# example 2 --------------------------------------------------------------
 circos.par(gap.degree = 0, track.margin = rep(0, 4), cell.padding = rep(0, 4), gap.after = 0)
 circos.initialize(factors = "a", xlim = c(1, 100))
 circos.track(factors = "a", ylim = c(-1, 1), 
@@ -43,13 +47,10 @@ circos.track(factors = "a", ylim = c(-1, 1),
                      col = "blue"
                  )
              })
+circos.clear()
 
 
-circos.initialize(factors = letters[1:4], xlim = c(0, 1))
-circos.track(factors = letters[1:4], ylim = c(0, 1), 
-             circos.text(x = ))
-
-
+# example 3 -----------------------------------------------------------------
 n = 1000
 df <- data.frame(fa = sample(letters[1:8], n, T), 
                  x = rnorm(n), 
@@ -91,11 +92,36 @@ circos.track(factors = df$fa, x = df$x, y = df$y,
 circos.clear()
 
 
+# example 4 --------------------------------------------------
+time <- c(8, 4, 2, 1)
+names(time) <- c("G1", "S", "G2", "M")
+cols <- RColorBrewer::brewer.pal(n = 4, name = "Set1")
+names(cols) <- c("G1", "S", "G2", "M")
+
+circos.par(start.degree = 90, track.height = 0.15, gap.after = 1, 
+           canvas.xlim = c(-1.3, 1.3), canvas.ylim = c(-1.3, 1.3))
+circos.initialize(factors = c("G1", "S", "G2", "M"), xlim = cbind(0, time))
+circos.track(c("G1", "S", "G2", "M"), ylim = c(0, 1), x = time, 
+             panel.fun = function(x, y){
+                 circos.arrow(x1 = 0, 
+                              x2 = x, 
+                              y = 0.5, 
+                              arrow.head.length = 0.25, 
+                              arrow.head.width = 1.5, 
+                              width = 1, 
+                              col = cols[get.current.sector.index()])
+                 
+                 circos.text(x = CELL_META$xcenter, 
+                             y = CELL_META$ycenter + uy(10, "mm"), 
+                             labels = CELL_META$sector.index)
+                 
+                 circos.axis(h = "top", labels.cex = 0.6)
+             }, bg.border = NA)
+
+circos.clear()
 
 
-
-
-
+# example 5 -----------------------------------------------------
 
 
 
