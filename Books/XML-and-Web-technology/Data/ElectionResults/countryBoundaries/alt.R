90a6b65c1508dda5aeeac0b3098e7f770ba91bf9
YinCY <yinchunyou2012@163.com>
2020-11-05 10:21:52 +0800

    remove auto

--- Books/XML-and-Web-technology/Data/ElectionResults/countryBoundaries/alt.R
+++
@@ -1,3 +0,0 @@
-k = map('county')
-stateId = gsub(",.*$", "", k$names)
-countyNames = tapply( gsub(".*,", "", k$names), stateId, function(x) x) 
--- Books/XML-and-Web-technology/Data/eBay/shopping.R
+++
@@ -1,9 +0,0 @@
-# http://open.api.ebay.com/shopping?callname=FindItems&responseencoding=XML&appid=YourAppIDHere&siteid=0&version=517&QueryKeywords=harry%20potter%20phoenix&MaxEntries=50
-
-getForm("http://open.api.ebay.com/shopping", responseencoding = "XML",
-   appid='UCDAVISS1IUSCQ7IP3158BKE848MZJ',
-   siteid = 0,
-   version = 517,
-   QueryKeywords= "harry potter phoenix",
-   MaxEntries = 50, .opts = list(verbose = TRUE))
-
