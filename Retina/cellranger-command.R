2e8e2bce10ea2695697249ad221b0371b81e1116
YinCY123 <yinchunyou2012@163.com>
2021-06-30 22:22:30 +0800

    removed

--- Retina/cellranger-command.R
+++
@@ -1,4 +0,0 @@
-
-cellranger count  --id=run_count_macular_cd31_donor7 --fastqs=/home/yincy/git/Data/AMD-related/AMD-single-cell-choroid-endothelial/fastq/donor7 --sample=macula_donor_7 --transcriptome=/home/yincy/git/Data/genome/human/10X/refdata-cellranger-GRCh38-3.0.0
-
-cellranger aggr --id=aggre_donor_4_7 --csv=aggre.csv
