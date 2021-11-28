5de5c15b231053d873718895759cd588b3b4b9b8
YinCY <yinchunyou2012@163.com>
2021-11-28 21:03:26 +0800

    removed

--- OOP/R-Object-oriented-Programming/simpleExecute.R
+++
@@ -1,8 +0,0 @@
-x <- as.double(readline(prompt = "what is the value of x? "))
-cat("I got the number ", format(x, digits = 6), ".\n")
-if(x < 0){
-    print("why would you give me a negative number?")
-    x <- abs(x)
-}
-
-y <- sqrt(x)
