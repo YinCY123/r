25b12866e9ad8e0841f09afb8eb6fa8c6eb56fc2
YinCY <yinchunyou2012@163.com>
2022-11-24 08:46:39 +0800

    base

---
+++ R_basics/R-Objects.R
@@ -0,0 +1,15 @@
+# R has five basic or 'atomic' classes of objects:
+
+1. character
+2. numeric (real numbers)
+3. interger
+4. complex
+5. logical (TRUE/FALSE)
+
+
+# Missing Values
+Missing values are denoted by `NA` or `NaN` for undefined mathematical operations.
+
+1. `NA` values have a class also, so there are integer `NA`, character `NA`, etc.
+
+2. A `NaN` value is also `NA` but the converse is not true.
